1ab4479339439e799fb14aba3293f82bbbe8eb96
Aslan <aslan.askarbek@inbox.ru>
2022-07-14 16:42:12 +0600

    Update parsing.py

--- parsing.py
+++ parsing.py
@@ -2,6 +2,7 @@
 from re import sub
 from typing import List
 import json
+
 import pydantic
 import requests
 import numpy as np
@@ -10,6 +11,7 @@ from selenium.webdriver import Chrome
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 
@@ -39,7 +41,9 @@ class Player(pydantic.BaseModel):
     win_rate: float
     hours_played: int
     show_offName: bool
-
+    emblem_isRare:bool
+class PlayerList(pydantic.BaseModel):
+    players: List[Player]
 
     
 
@@ -135,15 +139,56 @@ def getAllEmblems():
         file.close()
 
                
-def getEmblemRarity_sampleBased(sample:List[Player]):
+def getEmblemRarity_sampleBased(sample_steam_id:List[str],SignLevel:float=3.5):
     try:
         emblem_file=open('emblems.json','r')  
     except: #if warmind was updated with new emblems - just delete the current file [no user interface, but maybe i'll make it]
         getAllEmblems()
         emblem_file=open('emblems.json','r')
 
-    emblems=emblem_file.read()   
+    emblems=emblemList.parse_obj(json.loads(emblem_file.read()))  
+    players=[]
+    for steam_id in sample_steam_id:
+        urlDT_overview=f"https://destinytracker.com/destiny-2/profile/steam/{steam_id}/overview"
+        request_overview=requests.get(urlDT_overview)
+        soup_overview= BeautifulSoup(request_overview.text,'lxml')
 
+        
+
+        nickname=soup_overview.find('span',class_='trn-ign__username').text.strip('\n')
+        nickname=nickname[:nickname.find('\n')].strip()
+
+        numViews=soup_overview.find('div',class_='ph-details__subtitle').find_all('span')[1]
+        numViews=getNumbersInt(numViews)  #damn optimization 
+
+        id_bungie=soup_overview.find('div','ph-details__identifier').find('span','ph-details__name').find('span','trn-ign__discriminator')
+        id_bungie=getNumbersInt(id_bungie)
+
+        kd_overall=float(soup_overview.find('div','segment-stats regular-stats card bordered header-bordered responsive').find('div','stat align-left expandable').find('span','value').text)
+        win_rate=float(soup_overview.find('div','segment-stats regular-stats card bordered header-bordered responsive').findAll('div','stat align-left expandable')[2].find('span','value').text.replace('%',''))
+
+        hoursPlayed=soup_overview.find('div','segment-stats').findAll('span','matches')[1]
+        hoursPlayed=getNumbersInt(hoursPlayed)
+
+        show_off= bool(nickname.__len__()<=6)
+
+        urlBungie='https://www.bungie.net/en/Profile/GameHistory/3/'+steam_id
+        soupBungie=BeautifulSoup(requests.get(urlBungie).text,'lxml') 
+
+        emblem=str(soupBungie.find('div','select-option js-option-selectable').find('div','icon'))
+        emblem='https://bungie.net'+emblem[48:len(emblem)-10]
+        
+        emblem_rarity=next((it for it in emblems.emblems if it.src==emblem), None).rarity<SignLevel
+        players.append(Player(prime_steam_id=steam_id,nick_name=nickname,id_bungie=id_bungie,views=numViews,kd_overall=kd_overall, win_rate=win_rate,hours_played=hoursPlayed, show_offName=show_off,emblem_isRare=emblem_rarity))
+    
+    PlayersDataSet=open('players_dataset.json','w+')
+    PlayersDataSet.write(PlayerList(players=players).json())
+    PlayersDataSet.close()
+        
+
+
+
+    
 
 
 
@@ -171,11 +216,12 @@ def setPlayer(steam_id:str):
 
         show_off= bool(nickname.__len__()<=6) #defining show-off guy is a guestion to ML, here i define it as just short nickname
 
-        return Player(prime_steam_id=prime_steam_id,nick_name=nickname,id_bungie=id_bungie,views=numViews,kd_overall=kd_overall, win_rate=win_rate,hours_played=hoursPlayed, show_offName=show_off)
+        return Player(prime_steam_id=prime_steam_id,nick_name=nickname,id_bungie=id_bungie,views=numViews,kd_overall=kd_overall, win_rate=win_rate,hours_played=hoursPlayed, show_offName=show_off,emblem_isRare=getEmblemRarity(steam_id))
         
     
 # print(getEmblemRarity('4611686018493562197',3.5)) 
 #getAllEmblems()
+getEmblemRarity_sampleBased(['4611686018493562197'])
 
 # try:
 #     file=open('players.json','a')
